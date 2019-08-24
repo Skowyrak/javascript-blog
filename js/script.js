72aae91b098f29b71b79076c1208356bd65fd0b4
Kasia Skowyra <skowyra.kasia@gmail.com>
2019-08-24 20:36:51 +0200

    changes in clickedElement

--- js/script.js
+++ js/script.js
@@ -3,38 +3,10 @@
 //  console.log('links:', links);
 //})
 const titleClickHandler = function(event){
-  console.log(event);
-
-  /* remove class 'active' from all article links  */
-  const activeLinks = document.querySelectorAll('.titles a.active');
-
-  for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
-  }
-  /* add class 'active' to the clicked link */
-
-  /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.titles active');
-
-  for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
-  }
-  /* get 'href' attribute from the clicked link */
-
-  /* find the correct article using the selector (value of 'href' attribute) */
-
-  /* add class 'active' to the correct article */
-}
-
-const links = document.querySelectorAll('.titles a');
-
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
-}
-const titleClickHandler = function(event){
   const clickedElement = this;
-  console.log('Link was clicked!');
   clickedElement.classList.add('active');
+  event.preventDefault();
+  console.log('Link was clicked!');
 
   /* [DONE] remove class 'active' from all article links  */
 
