7aba540dffbffdb2a1c1fd39512869e74769ebec
Kasia Skowyra <skowyra.kasia@gmail.com>
2019-09-17 20:46:49 +0200

    change in tags

--- js/script.js
+++ js/script.js
@@ -178,7 +178,7 @@ function generateTags() {
   /* [NEW] START LOOP: for each tag in allTags: */
   for(let tag in allTags) {
     /*[NEW] generate code of a link and add it to allTagsHTML */
-    allTagsHTML += '<li><a href="#tag-' + tag + '" > ' + tag + ' </a></li>';
+    allTagsHTML += '<li><a href="#tag-' + tag + '"> ' + tag  + '(' + allTags[tag] + ') </a></li>';
   }
   /*[NEW] END LOOP */
   /*[NEW] add html form allTagsHTML to tag list */
